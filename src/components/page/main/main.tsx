a5025983cbc06475ebf03aa4fdaa14423fbfc856
github akito-10 <akt.fukuda@gmail.com>
2023-09-26 00:16:52 +0900

    💄 コンポーネント追加

--- src/components/page/main/main.tsx
+++ src/components/page/main/main.tsx
@@ -4,6 +4,10 @@ import "regenerator-runtime";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const MainPage = () => {
   const {
@@ -14,24 +18,38 @@ export const MainPage = () => {
   } = useSpeechRecognition();
 
   if (!browserSupportsSpeechRecognition) {
-    return <span>Browser does not support speech recognition.</span>;
+    return <main>Browser does not support speech recognition.</main>;
   }
 
   return (
-    <main className="w-full min-h-screen flex items-center justify-center flex-col gap-4">
-      <p>Microphone: {listening ? "on" : "off"}</p>
-      <button
-        onClick={() =>
-          SpeechRecognition.startListening({
-            continuous: true,
-          })
-        }
-      >
-        Start
-      </button>
-      <button onClick={() => SpeechRecognition.stopListening()}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+    <main className="w-full min-h-screen flex items-center justify-center flex-col">
+      <h1 className="text-xl mb-2">マイク</h1>
+      <div className="flex items-center space-x-2 mb-8">
+        <Switch
+          id="airplane-mode"
+          onCheckedChange={() => {
+            if (listening) {
+              SpeechRecognition.stopListening();
+            } else {
+              SpeechRecognition.startListening({
+                continuous: true,
+              });
+            }
+          }}
+        />
+        <Label htmlFor="airplane-mode">{listening ? "オン" : "オフ"}</Label>
+      </div>
+      <Card className="max-w-full w-96 my-4">
+        <CardHeader>
+          <CardTitle className="text-center">出力</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{transcript}</p>
+        </CardContent>
+      </Card>
+      <Button variant="outline" onClick={resetTranscript}>
+        Reset
+      </Button>
     </main>
   );
 };
