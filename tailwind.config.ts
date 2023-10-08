7e1c96dcd57e1a9eea3f8532f597c17cf8405438
github akito-10 <akt.fukuda@gmail.com>
2023-10-08 18:24:30 +0900

    色を調整

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,17 +17,21 @@ module.exports = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
+        border: "var(--border)",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: "var(--background)",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        tertiary: {
+          DEFAULT: "var(--tertiary)",
           foreground: "hsl(var(--secondary-foreground))",
         },
         destructive: {
