25084d72d3cfdec80434536622349cb737d49177
github akito-10 <akt.fukuda@gmail.com>
2023-10-15 00:36:13 +0900

    asset

---
+++ src/components/assets/list.tsx
@@ -0,0 +1,16 @@
+export const ListIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="12"
+      viewBox="0 0 16 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 7.2H1.77778V4.8H0V7.2ZM0 12H1.77778V9.6H0V12ZM0 2.4H1.77778V0H0V2.4ZM3.55556 7.2H16V4.8H3.55556V7.2ZM3.55556 12H16V9.6H3.55556V12ZM3.55556 0V2.4H16V0H3.55556Z"
+        fill="black"
+      />
+    </svg>
+  );
+};
