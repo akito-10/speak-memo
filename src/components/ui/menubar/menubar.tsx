81d011b963b0a4d5791625326075fd471fa9761e
github akito-10 <akt.fukuda@gmail.com>
2023-10-16 00:48:14 +0900

    create menubar

--- src/components/ui/menubar/menubar.tsx
+++ src/components/ui/menubar/menubar.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { ListIcon } from "@/components/assets/list";
 import { PlusIcon } from "@/components/assets/plus";
 import { SearchIcon } from "@/components/assets/search";
 import { Button } from "@/components/shadcn/ui/button";
+import Link from "next/link";
+import { FC } from "react";
+import { useSearchParams } from "next/navigation";
 
-export const Menubar = () => {
+export const Menubar: FC = () => {
+  const searchParams = useSearchParams();
+  const selectedMemoId = searchParams.get("memoId")
+    ? Number(searchParams.get("memoId"))
+    : undefined;
+
   return (
     <header className="w-40 h-screen border-r bg-bgMain">
       <div className=" border-b p-4 flex flex-col gap-4">
@@ -21,7 +31,25 @@ export const Menubar = () => {
           <PlusIcon />
         </Button>
       </h3>
-      <ul></ul>
+      <ul className="mt-2 flex flex-col">
+        {[
+          { id: 1, title: "memo A" },
+          { id: 2, title: "memo B" },
+          { id: 3, title: "memo C" },
+        ].map((memo) => (
+          <li key={memo.id}>
+            <Link
+              className="py-1 px-6 w-full h-full rounded-none justify-start block hover:bg-accent"
+              href={`/?memoId=${memo.id}`}
+              style={{
+                color: selectedMemoId === memo.id ? "#7cb855" : "inherit",
+              }}
+            >
+              {memo.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </header>
   );
 };
